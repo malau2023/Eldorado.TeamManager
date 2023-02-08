d1ce8c53f641b76dc1d1972150a502d07552a8db
mariella pugens <mariellapugens@gmail.com>
2023-02-08 10:28:41 -0300

    implementação de entidades incompleto

---
+++ src/Eldorado.TeamManager.Domain/Entities/Collaborator.cs
@@ -0,0 +1,43 @@
+using Eldorado.TeamManager.Domain.Enums;
+using System.Collections.Generic;
+
+//namespace Eldorado.TeamManager.Domain.Entities
+//{
+
+  //  public class Collaborator : BaseEntity
+    //{
+      //  public Collaborator() { }
+
+        //public Collaborator(string Name, string RG, DateTime BirthDate, string Email, string Observation, List<CollaboratorSkill> Skill)
+        //{
+           // Name = name;
+            //RG = rg;
+            //BirthDate = birthDate;
+            //Email = email;
+            //Observation = observation;
+            //Skill = skill;
+
+        //}
+
+        //public String Name { get; set; }
+        //public String RG { get; set; }
+        //public DateTime BirthDate { get; set; }
+        //public String Email { get; set; }
+        //public String Observation { get; set; }
+        //public List<CollaboratorSkill> Skill { get; set; }
+
+       // public void Edit(string Name, string RG, DateTime BirthDate, string Email, string Observation, List<CollaboratorSkill> Skill)
+        //{
+          //  Name = name;
+           // RG = rg;
+            //BirthDate = birthDate;
+           // Email = email;
+            //Observation = observation;
+            //Skill = skill;
+
+        //}
+
+
+
+   // }
+//}
---
+++ src/Eldorado.TeamManager.Domain/Entities/CollaboratorSkill.cs
@@ -0,0 +1,22 @@
+//using Eldorado.TeamManager.Domain.Enums;
+
+
+//namespace Eldorado.TeamManager.Domain.Entities
+//{
+  //  public class CollaboratorSkill : BaseEntity
+    //{
+       // public CollaboratorSkill() { }
+
+       // public CollaboratorSkill()
+       // {
+
+       // }
+      //  public long CodColaborator { get; set; }
+
+       // public virtual Collaborator Collaborator { get; set; }
+
+       // public long CodSkill { get; set; }
+
+       // public virtual Skill { get; set; }
+//}
+//}
