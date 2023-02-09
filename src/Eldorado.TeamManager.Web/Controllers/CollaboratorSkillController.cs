6dcccbf34718c284179874de22c0fea501e079d4
adriana.ribeiro <ak_ribeiro@yahoo.com.br>
2023-02-09 16:07:33 -0300

    Teste

---
+++ src/Eldorado.TeamManager.Web/Controllers/CollaboratorSkillController.cs
@@ -0,0 +1,12 @@
+//using Microsoft.AspNetCore.Mvc;
+
+//namespace Eldorado.TeamManager.Web.Controllers
+//{
+    //public class CollaboratorSkillController : Controller
+    //{
+       // public IActionResult Index()
+       // {
+           // return View();
+       // }
+   // }
+//}
